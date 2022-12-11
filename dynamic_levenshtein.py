500aab0ceb0ede86cd70e56e119660c336b30b67
Ступеньков Валерий <Valery.Stupenkov@fors.ru>
2022-12-11 15:07:47 +0300

    Добавил построение результирующей строки шаблона со значениями переменных

--- dynamic_levenshtein.py
+++ dynamic_levenshtein.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import unravel_index
 
 def calc_lev_with_blanks(s1, s2):
     n = len(s1)
@@ -16,12 +17,12 @@ def calc_lev_with_blanks(s1, s2):
     for line in F:
         print(line)
 
-    vars = {"X": "", "Y":"", "Z":""}
+    vars = {"X": ("", ""), "Y": ("", ""), "Z": ("", "")}
+    rule = build_rule(s1, s2, F, vars, True)
+    print(rule)
+    print(vars)
 
-    for i in range(n):
-        for j in range(m):
-            if F[i][j] != 0:
-                break
+
 
 
 def calc_lev_without_blanks(s1, s2):
@@ -43,8 +44,75 @@ def calc_lev_without_blanks(s1, s2):
     for line in F:
         print(line)
 
+    vars = {"X": ("", ""), "Y":("", ""), "Z":("", "")}
+    rule = build_rule(s1, s2, F, vars, False)
+    print(rule)
+    print(vars)
+
+def build_rule(s1, s2, F, vars, blanks):
+    n = len(s1)
+    m = len(s2)
+    prev_i = 0
+    prev_j = 0
+    res = ""
+    i = 0
+    j = 0
+    while i < n and j < m:
+        if F[i][j] != 0:
+            if i == 0 or j == 0:
+                res += s1[i]
+                i, j = find_max_pos(F[i+1:, j+1:], n, m, blanks)
+            elif (i == n-2 or j == m-2) or (i == n-1 or j == m-1):
+                if i != prev_i+1 or j != prev_j+1:
+                    for key in vars.keys():
+                        if vars[key] == ("", ""):
+                            if i != prev_i + 1:
+                                vars[key] = (s1[prev_i+1:i], "")
+                            if j != prev_j + 1:
+                                vars[key] = (vars[key][0], s2[prev_j+1:j])
+                            res += key
+                            break
+                res += s1[i]
+                prev_i, prev_j = i, j
+                i += 1
+                j += 1
+            elif i == prev_i+1 and j == prev_j+1:
+                res += s1[i]
+                prev_i, prev_j = i, j
+                i, j = find_max_pos(F[i + 1:, j + 1:], n, m, blanks)
+            elif i != prev_i+1 or j != prev_j+1:
+                for key in vars.keys():
+                    if vars[key] == ("", ""):
+                        if i != prev_i+1:
+                            vars[key] = (s1[prev_i+1:i], "")
+                        if j != prev_j+1:
+                            vars[key] = (vars[key][0], s2[prev_j+1:j])
+                        res += key
+                        break
+                res += s1[i]
+                prev_i, prev_j = i, j
+                i, j = find_max_pos(F[i + 1:, j + 1:], n, m, blanks)
+    return res
+
+
+
+def find_max_pos(a, n, m, blanks):
+    if blanks:
+        i, j = unravel_index(a.argmax(), a.shape)
+    else:
+        tmp = a[1:, 1:]
+        if a[0][0] > tmp.max():
+            i, j = 0, 0
+        else:
+            i, j = unravel_index(tmp.argmax(), tmp.shape)
+            i += 1
+            j += 1
+    shape = a.shape
+    i += n-shape[0]
+    j += m-shape[1]
+    return i, j
 
 
 #calc_lev_with_blanks("гомеопатия", "геометрия")
-#calc_lev_without_blanks("гомеопатия", "геометрия")
-calc_lev_without_blanks("баXан", "баклан")
+calc_lev_without_blanks("гомеопатия", "геометрия")
+#calc_lev_without_blanks("баXан", "баклан")
