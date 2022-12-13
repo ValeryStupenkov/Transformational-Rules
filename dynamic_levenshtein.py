aa6756c9bc57de046a287e7679cbcda5f8ee432a
Ступеньков Валерий <Valery.Stupenkov@fors.ru>
2022-12-13 20:40:18 +0300

    Добавил построение всех макс путей

--- dynamic_levenshtein.py
+++ dynamic_levenshtein.py
@@ -17,10 +17,9 @@ def calc_lev_with_blanks(s1, s2):
     for line in F:
         print(line)
 
-    vars = {"X": ("", ""), "Y": ("", ""), "Z": ("", "")}
-    rule = build_rule(s1, s2, F, vars, True)
-    print(rule)
-    print(vars)
+    rules = build_rules(s1, s2, F, vars, True)
+    print(rules)
+    #print(vars)
 
 
 
@@ -44,25 +43,51 @@ def calc_lev_without_blanks(s1, s2):
     for line in F:
         print(line)
 
-    vars = {"X": ("", ""), "Y":("", ""), "Z":("", "")}
-    rule = build_rule(s1, s2, F, vars, False)
-    print(rule)
-    print(vars)
+    rules = build_rules(s1, s2, F, vars, False)
+    print(rules)
+    #print(vars)
 
-def build_rule(s1, s2, F, vars, blanks):
+def build_rules(s1, s2, F, vars, blanks):
     n = len(s1)
     m = len(s2)
     prev_i = 0
     prev_j = 0
-    res = ""
+    results = {}
     i = 0
     j = 0
+    counted_lev_distance = max(map(max, F))
+    for i in range(n):
+        for j in range(m):
+            if F[i][j] == counted_lev_distance:
+                vars = {"X": ("", ""), "Y": ("", ""), "Z": ("", "")}
+                res = build_rule(s1, s2, F, vars, blanks, i, j)
+                results[res] = vars
+    return results
+
+def build_rule(s1, s2, F, vars, blanks, i, j):
+    n = len(s1)
+    m = len(s2)
+    prev_i = i
+    prev_j = j
+    res = ""
     while i < n and j < m:
         if F[i][j] != 0:
-            if i == 0 or j == 0:
+            # максимальный элемент
+            if i == prev_i or j == prev_j:
+                if i != 0 or j != 0:
+                    for key in vars.keys():
+                        if vars[key] == ("", ""):
+                            if i != 0:
+                                vars[key] = (s1[0:i], "")
+                            if j != 0:
+                                vars[key] = (vars[key][0], s2[0:j])
+                            res += key
+                            break
                 res += s1[i]
                 i, j = find_max_pos(F[i+1:, j+1:], n, m, blanks)
+            # проверка чтобы не выходить за границы
             elif (i == n-2 or j == m-2) or (i == n-1 or j == m-1):
+                # случай для заполнения переменных
                 if i != prev_i+1 or j != prev_j+1:
                     for key in vars.keys():
                         if vars[key] == ("", ""):
@@ -76,6 +101,7 @@ def build_rule(s1, s2, F, vars, blanks):
                 prev_i, prev_j = i, j
                 i += 1
                 j += 1
+            # случай для заполнения переменных
             elif i == prev_i+1 and j == prev_j+1:
                 res += s1[i]
                 prev_i, prev_j = i, j
@@ -113,6 +139,6 @@ def find_max_pos(a, n, m, blanks):
     return i, j
 
 
-#calc_lev_with_blanks("гомеопатия", "геометрия")
+calc_lev_with_blanks("гомеопатия", "геометрия")
 calc_lev_without_blanks("гомеопатия", "геометрия")
 #calc_lev_without_blanks("баXан", "баклан")
