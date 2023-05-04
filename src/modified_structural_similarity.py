5204393b150142380bbd5a73982eef4c00fdd9e6
Ступеньков Валерий <Valery.Stupenkov@fors.ru>
2023-05-04 19:47:30 +0300

    Рефакторинг добавления переменных при вычислении структурного сходства

--- src/modified_structural_similarity.py
+++ src/modified_structural_similarity.py
@@ -1,10 +1,10 @@
+variables = ["X", "Y", "Z", "A", "B", "C", "D", "E", "F",
+             "G", "H", "I", "J", "K", "L", "M", "N", "O",
+             "P", "Q", "R", "S", "T", "U", "V", "W"]
 
 def build_iterations(s1, s2, F):
     n = len(s1)
     m = len(s2)
-    vars = {"X": ("", ""), "Y": ("", ""), "Z": ("", ""),
-            "A": ("", ""), "B": ("", ""), "C": ("", ""),
-            "D": ("", ""), "E": ("", ""), "F": ("", ""), "G": ("", "")}
     results = {}
     begins = find_max_poses(F, n, m)
     samples = {}
@@ -14,7 +14,7 @@ def build_iterations(s1, s2, F):
         prev_j = 0
         max_i = prev_i
         max_j = prev_j
-        tmp_vars = vars.copy()
+        tmp_vars = {}
         first_step_res = build_rule_step("", s1, s2, tmp_vars, prev_i, prev_j, begin)
         results[first_step_res] = [begin, tmp_vars]
 
@@ -27,15 +27,12 @@ def build_iterations(s1, s2, F):
                 if prev_i == n-1 or prev_j == m-1:
                     # Добавить переменную в конец в случае необходимости
                     if prev_i < n-1 or prev_j < m-1:
-                        new_key = ""
-                        for k in results[key][1].keys():
-                            if results[key][1][k] == ("", ""):
-                                new_key = key + k
-                                if prev_i == n-1:
-                                    results[key][1][k] = ("", s2[prev_j+1:])
-                                elif prev_j == m-1:
-                                    results[key][1][k] = (s1[prev_i+1:], "")
-                                break
+                        k = new_variable(results[key][1])
+                        new_key = key + k
+                        if prev_i == n - 1:
+                            results[key][1][k] = ("", s2[prev_j + 1:])
+                        elif prev_j == m - 1:
+                            results[key][1][k] = (s1[prev_i + 1:], "")
                         tmp_dict[new_key] = [[results[key][0][0]+1, results[key][0][1]+1], results[key][1]]
                     else:
                         tmp_dict[key] = [[results[key][0][0] + 1, results[key][0][1] + 1], results[key][1]]
@@ -51,25 +48,19 @@ def build_iterations(s1, s2, F):
 
                     # условия для остановки вычислений
                     if max[0] != n and max[1] != m and F[max[0]][max[1]] == 0:
-                        for k in tmp_vars.keys():
-                            if tmp_vars[k] == ("", ""):
-                                tmp_vars[k] = (s1[prev_i+1:], s2[prev_j+1:])
-                                tmp_dict[key+k] = [[n, m], tmp_vars]
-                                break
+                        k = new_variable(tmp_vars)
+                        tmp_vars[k] = (s1[prev_i + 1:], s2[prev_j + 1:])
+                        tmp_dict[key + k] = [[n, m], tmp_vars]
                         continue
                     elif max[0] != n and max[1] == m:
-                        for k in tmp_vars.keys():
-                            if tmp_vars[k] == ("", ""):
-                                tmp_vars[k] = (s1[max[0]:], "")
-                                tmp_dict[key+k] = [[n, m], tmp_vars]
-                                break
+                        k = new_variable(tmp_vars)
+                        tmp_vars[k] = (s1[max[0]:], "")
+                        tmp_dict[key + k] = [[n, m], tmp_vars]
                         continue
                     elif max[0] == n and max[1] != m:
-                        for k in tmp_vars.keys():
-                            if tmp_vars[k] == ("", ""):
-                                tmp_vars[k] = ("", s2[max[1]:])
-                                tmp_dict[key+k] = [[n, m], tmp_vars]
-                                break
+                        k = new_variable(tmp_vars)
+                        tmp_vars[k] = ("", s2[max[1]:])
+                        tmp_dict[key + k] = [[n, m], tmp_vars]
                         continue
 
                     step_res = build_rule_step(key, s1, s2, tmp_vars, prev_i, prev_j, max)
@@ -86,8 +77,7 @@ def build_iterations(s1, s2, F):
         for key in results:
             samples[key] = results[key]
 
-    #print(results)
-    # Перевод из внутреннего представления и склеивание переменных
+    # Перевод из внутреннего представления
     rules = {}
     for key in samples.keys():
         tmp_v = samples[key][1]
@@ -118,19 +108,16 @@ def build_rule_step(res, s1, s2, vars, prev_i, prev_j, max):
     i = max[0]
     j = max[1]
     # максимальный элемент
-    if i == prev_i or j == prev_j:
+    if prev_i == 0 and prev_j == 0:
         if i != 0 or j != 0:
-            # цикл добавления переменной в случае если начало не совпадает
-            for key in vars.keys():
-                if vars[key] == ("", ""):
-                    if i != 0:
-                        #vars[key] = (delete_variables(s1[0:i]), "")
-                        vars[key] = (s1[0:i], "")
-                    if j != 0:
-                        #vars[key] = (vars[key][0], delete_variables(s2[0:j]))
-                        vars[key] = (vars[key][0], s2[0:j])
-                    res += key
-                    break
+            # добавление переменной в случае если начало не совпадает
+            key = new_variable(vars)
+            vars[key] = ("", "")
+            if i != 0:
+                vars[key] = (s1[0:i], "")
+            if j != 0:
+                vars[key] = (vars[key][0], s2[0:j])
+            res += key
         if res != "":
             last_char = res[-1]
         else:
@@ -139,27 +126,20 @@ def build_rule_step(res, s1, s2, vars, prev_i, prev_j, max):
             if last_char.isupper():
                 vars[last_char] = (vars[last_char][0] + s1[i], vars[last_char][1] + s2[j])
             else:
-                for key in vars.keys():
-                    if vars[key] == ("", ""):
-                        # vars[key] = ("", delete_variables(s2[j]))
-                        vars[key] = (s1[i], s2[j])
-                        res += key
-                        break
+                key = new_variable(vars)
+                vars[key] = (s1[i], s2[j])
+                res += key
         else:
             res += s2[j]
 
     # Случай, при котором всё ок
     elif i == prev_i + 1 and j == prev_j + 1:
         if s1[i].isupper():
-            for key in vars.keys():
-                if vars[key] == ("", ""):
-                    # vars[key] = ("", delete_variables(s2[j]))
-                    vars[key] = (s1[i], s2[j])
-                    res += key
-                    break
+            key = new_variable(vars)
+            vars[key] = (s1[i], s2[j])
+            res += key
         else:
             res += s2[j]
-        # конец эксп части
 
     # случай для заполнения переменных (обычный)
     elif i != prev_i + 1 or j != prev_j + 1:
@@ -170,22 +150,17 @@ def build_rule_step(res, s1, s2, vars, prev_i, prev_j, max):
             last_char = " "
         if last_char.isupper():
             if i != prev_i + 1:
-                # vars[key] = (delete_variables(s1[prev_i + 1:i]), "")
                 vars[last_char] = (vars[last_char][0] + s1[prev_i + 1:i], vars[last_char][1])
             if j != prev_j + 1:
-                # vars[key] = (vars[key][0], delete_variables(s2[prev_j + 1:j]))
                 vars[last_char] = (vars[last_char][0], vars[last_char][1] + s2[prev_j + 1:j])
         else:
-            for key in vars.keys():
-                if vars[key] == ("", ""):
-                    if i != prev_i + 1:
-                        #vars[key] = (delete_variables(s1[prev_i + 1:i]), "")
-                        vars[key] = (s1[prev_i + 1:i], "")
-                    if j != prev_j + 1:
-                        #vars[key] = (vars[key][0], delete_variables(s2[prev_j + 1:j]))
-                        vars[key] = (vars[key][0], s2[prev_j + 1:j])
-                    res += key
-                    break
+            key = new_variable(vars)
+            vars[key] = ("", "")
+            if i != prev_i + 1:
+                vars[key] = (s1[prev_i + 1:i], "")
+            if j != prev_j + 1:
+                vars[key] = (vars[key][0], s2[prev_j + 1:j])
+            res += key
         # Здесь в конце res всегда переменная, так что просто дописываем в неё
         last_char = res[-1]
         if s1[i].isupper():
@@ -195,27 +170,10 @@ def build_rule_step(res, s1, s2, vars, prev_i, prev_j, max):
     return res
 
 
-# Удаление переменных из строки
-def delete_variables(str):
-    res = ""
-    for i in range(len(str)):
-        if not str[i].isupper():
-            res += str[i]
-    return res
-
-# Удаление дубликатов переменных
-def replace_equal_vars(sample, vars):
-    res = ""
-    n = len(sample)
-    for i in reversed(range(0, n)):
-        if sample[i].isupper():
-            for key in vars.keys():
-                if vars[key] != ("", "") and vars[key] == vars[sample[i]] and key != sample[i]:
-                    res += key
-                    vars[sample[i]] = ("", "")
-            if vars[sample[i]] == ("", ""):
-                continue
-        res += sample[i]
-
-    return res[::-1]
+def new_variable(vars):
+    for key in variables:
+        if not key in vars:
+            return key
+        else:
+            continue
 
